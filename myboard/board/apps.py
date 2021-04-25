43a14f1e1e399646289a54175f9674014543f9d4
Evgeny <eugen.eisner@gmail.com>
2021-04-25 13:35:56 +0200

    add signals for Comments

--- myboard/board/apps.py
+++ myboard/board/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class BoardConfig(AppConfig):
     name = 'board'
+
+    # нам надо переопределить метод ready,
+    # чтобы при готовности нашего приложения импортировался модуль со всеми функциями обработчиками
+    def ready(self):
+        import board.signals
